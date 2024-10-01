7718f5ab736ebba51b9fcfa0e9b963e43225c57a
EddieMartinez07 <142748564+EddieMartinez07@users.noreply.github.com>
2024-10-01 12:11:44 -0500

    .cs

---
+++ Vuelos Baratos a partir de una Base de Datos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Flight
+{
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+    public decimal Price { get; set; }
+    public DateTime Date { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var flights = new List<Flight>
+        {
+            new Flight { Origin = "Quito", Destination = "Guayaquil", Price = 150, Date = DateTime.Today },
+            new Flight { Origin = "Quito", Destination = "Guayaquil", Price = 100, Date = DateTime.Today.AddDays(1) },
+            new Flight { Origin = "Quito", Destination = "Guayaquil", Price = 120, Date = DateTime.Today.AddDays(2) }
+        };
+
+        var cheapestFlight = flights
+            .Where(f => f.Origin == "Quito" && f.Destination == "Guayaquil")
+            .OrderBy(f => f.Price)
+            .FirstOrDefault();
+
+        if (cheapestFlight != null)
+        {
+            Console.WriteLine($"Vuelo más barato de Quito a Guayaquil: Precio {cheapestFlight.Price}");
+        }
+    }
+}
